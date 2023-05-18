546e9dabb34db00efc0518036b95a03a9eb9d137
Catalunha <catalunha.mj@gmail.com>
2023-05-18 17:54:09 -0300

    Update init_back4app.dart

--- lib/app/data/b4a/init_back4app.dart
+++ lib/app/data/b4a/init_back4app.dart
@@ -6,8 +6,8 @@ class InitBack4app {
   }
 
   Future<bool> init() async {
-    const keyApplicationId = '41khDEwc9OeEDclYQ9UnT2HEmysZ2E5JG5534qsp';
-    const keyClientKey = 'Ha2Cr0rF2iHTCiJFyeadmwMY6wTVmDw4R9W2Mu6O';
+    const keyApplicationId = '';
+    const keyClientKey = '';
     const keyParseServerUrl = 'https://parseapi.back4app.com';
     await Parse().initialize(
       keyApplicationId,
